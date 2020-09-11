6c7aafc660f2629a10d40ff0a8a1e8a89e0f6fa4
Pavel <paveldy@gmail.com>
2020-09-11 12:07:11 +0300

    New

--- src/ru/geekbrains/java_two/lesson_fife/HomeWork_2_5/MiThread.java
+++ src/ru/geekbrains/java_two/lesson_fife/HomeWork_2_5/MiThread.java
@@ -2,7 +2,6 @@ package ru.geekbrains.java_two.lesson_fife.HomeWork_2_5;
 
 class MiThread extends Thread {
 
-
     static final int size = 10000000;
     static final int h = size / 2;
     static float[] arr = new float[size];
